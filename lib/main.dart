8a66586361573d85f97513dbbeea12f67edd8094
dave fang <i2659r32@gmail.com>
2026-03-30 10:08:46 +0800

    Initial commit: Add Flutter web project and GitHub Actions workflow

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World & Codeium', style: TextStyle(fontSize: 32)),
+        ),
+      ),
+    ),
+  );
+}
